c65058b2b26258090b6b8a913aaff3d32ae210c7
INTERGRUPO\jdboterog <jdboterog@intergrupo.com>
2018-04-02 15:23:24 -0500

    Displaying a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Thor'},
+    { id: 12, name: 'Iron Man'},
+    { id: 13, name: 'Hulk'},
+    { id: 14, name: 'Captain America'},
+    { id: 15, name: 'Ant-Man'},
+    { id: 16, name: 'Hawkeye'},
+    { id: 17, name: 'Quicksilver'},
+    { id: 18, name: 'Hercules'},
+    { id: 19, name: 'Scarlet Witch'},
+    { id: 20, name: 'Mantis'}
+];

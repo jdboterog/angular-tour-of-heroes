b0e49c9f1a70b0b04bdcd0150deacb8207937489
INTERGRUPO\jdboterog <jdboterog@intergrupo.com>
2018-04-03 09:55:19 -0500

    Services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message after fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');    
+    return of(HEROES); 
+  }
+  
+}
